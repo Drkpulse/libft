ec3d3f9fc7286cb29938cd67dff97ea60c44bc40
Beirão <phsicopt@gmail.com>
2023-10-19 09:47:37 +0100

    join&trim

--- Files/libft.h
+++ Files/libft.h
@@ -28,12 +28,12 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(void *str1, const void *str2, size_t n);
-void	*ft_memcpy(void *str2, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *str1, const void *str2, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
 char	*ft_strchr(const char *str, int c);
-size_t	ft_strlcat(char *str2, const char *src, size_t size);
-size_t	ft_strlcpy(char *str2, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strnstr(const char *string, const char *substring, size_t len);
@@ -45,5 +45,6 @@ char	*ft_strdup(const char *src);
 
 //NON LIBC FUNCTIONS//
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
